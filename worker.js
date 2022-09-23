07a68a972b71fb68e52714c828869eaf092c1e98
Nathan Clevenger <nateclev@gmail.com>
2022-09-23 04:14:52 -0500

    Update worker.js

--- worker.js
+++ worker.js
@@ -18,7 +18,7 @@ export default {
   fetch: async (req, env) => {
     const { user, origin, requestId, method, body, time, pathSegments, query } = await env.CTX.fetch(req).then(res => res.json())
     let [action, target] = pathSegments
-    let url = 'https://' + (target == ':url' ? 'json.fyi/northwind.json' : target)
+    let url = 'https://' + !target || target == ':url' ? 'json.fyi/northwind.json' : target
     const res = await fetch(url)
     await env.BUCKET.put(target, res.body)
     return new Response(JSON.stringify({ api, method, url, user }, null, 2), { headers: { 'content-type': 'application/json; charset=utf-8' }})
