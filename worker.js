4a884a6d0e655e61f8e483c23ab5349b9ad56e9b
Nathan Clevenger <nateclev@gmail.com>
2022-09-23 03:30:55 -0500

    Create worker.js

---
+++ worker.js
@@ -0,0 +1,24 @@
+export const api = {
+  icon: '凵',
+  name: 'bucket.do',
+  description: 'Cloud Bucket API',
+  url: 'https://bucket.do/api',
+  type: 'https://apis.do/storage',
+  endpoints: {
+    import: 'https://bucket.do/import/:url',
+    export: 'https://bucket.do/export/:url',
+  },
+  site: 'https://bucket.do',
+  login: 'https://bucket.do/login',
+  signup: 'https://bucket.do/signup',
+  repo: 'https://github.com/drivly/bucket.do',
+}
+
+export default {
+  fetch: async (req, env) => {
+    const { user, origin, requestId, method, body, time, pathSegments, query } = await env.CTX.fetch(req).then(res => res.json())
+    let [method, target] = pathSegments
+    let url = 'https://' + target
+    return new Response(JSON.stringify({ api, method, url, user }, null, 2), { headers: { 'content-type': 'application/json; charset=utf-8' }})
+  }
+}
